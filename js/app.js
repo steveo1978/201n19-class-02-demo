aef01512cf84f1e69b899676b14d06c486816f3f
steveo1978 <steve.r.marical@outlook.com>
2020-07-25 15:24:02 -0700

    fixed mistakes in js

--- js/app.js
+++ js/app.js
@@ -10,16 +10,16 @@ alert(userName + ' Hello, Hi, Holla ! Welcome to the freakshow that is my mind !
 var question1 = prompt('I was born in Oklahoma, That part is true, but was I born in 1978 ? yes or no').toLowerCase();
 if (question1 === 'yes') {
   alert('NICE !!!');
-}  else if (question1 === 'no') {
+} else if (question1 === 'no') {
   alert('Wrong answer dude !!');
 }
 //console.log('question1' + user guess year born);
 
 //Question 2
 var question2 = prompt('Was I a door-to-door Salesman ? yes or no ?').toLowerCase();
-if (question2 == 'yes') {
+if (question2 === 'yes') {
   alert('You got it dude ! I was a door-to-door vaccume cleaner salesman');
-}  else if (question2 === 'no') {
+} else if (question2 === 'no') {
   alert('Sorry Charley, I was.');
 }
 //console.log('question2' + was I a salesman);
@@ -38,17 +38,17 @@ if (question3 === 'yes') {
 var question4 = prompt('Have I ever built a bomb ? yes or no ?').toLowerCase();
 if (question4 === 'yes') {
   alert('Right on homie, you answered that right !! I built bombs as an Aviation Ordnanceman for the US Navy.');
-}  else if (question4 === 'no') {
+} else if (question4 === 'no') {
   alert('Wrong !! Come on its not such a huge stretch for me to be a bomb builder.');
 }
-//console.log('question4' + was I a bomb builder); 
+//console.log('question4' + was I a bomb builder);
 
-//Question 5 
+//Question 5
 var question5 = prompt('Have I ever been fired from a job ? Please answer yes or no ?').toLowerCase();
 if (question5 === 'yes') {
   alert('Yup your right ! Ive been fired form every job Ive ever worked at except for the Navy. lol oops.');
 } else if (question5 === 'no') {
   alert('you loose, wrong answer. Ive lost every job Ive ever had with the exception of the military.');
 }
-      //console.log('question5.toLowerCase');
+//console.log('question5.toLowerCase');
 
